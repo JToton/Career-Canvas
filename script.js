f4220209409e83b7b812dc165488a5b364ab3ad4
JacobToton <Jake_Toton@live.com>
2024-03-14 05:38:32 -0600

    Projects Slideshow and Contact Info
    Added in a slide show to present recent projects.  Additionally started to work on the contact and footer sections at the bottom of the page. Updated some jpg's

--- script.js
+++ script.js
@@ -5,3 +5,42 @@ function toggleDropdown() {
   dropdown.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+//Slide Show Function
+//Function Sourced from https://w3schools.com/
+//commented to show understanding.
+let slideIndex = 1;
+showSlides(slideIndex);
+
+//function to iterate slides 'n+1'
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+//function to track current slide 'n'
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  //wrap to first if n+1 = more than total
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  //if <1 reset
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  //iteration for loops
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
