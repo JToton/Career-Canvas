7695b9ebd9a882244aeecb778dec9b5da252e223
JacobToton <Jake_Toton@live.com>
2024-03-14 23:39:37 -0600

    Removed old script and fixed merge error
    Removed old script and fixed merge error

--- script.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-  //function to control hamburger drop down menu
-  const dropdown = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  dropdown.classList.toggle("open");
-  icon.classList.toggle("open");
-}
